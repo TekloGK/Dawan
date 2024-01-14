eb94875e67089329b3e7c18e2e8e6a05fb1a16b5
TekloGK <154121704+TekloGK@users.noreply.github.com>
2024-01-14 21:51:51 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -61,7 +61,7 @@ non_queued_dl = set()
 non_queued_up = set()
 
 try:
-    if bool(environ.get('_____REMOVE_THIS_LINE_____')):
+    if bool(environ.get('')):
         error('README is there to be read! Read and try again! Exiting now!')
         exit()
 except:
@@ -77,14 +77,14 @@ download_dict = {}
 rss_dict = {}
 cached_dict = {}
 
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6970520093:AAFw3ih6RSIVQ-t3Mt_6PU9XlQfBwCIl2bQ')
 if len(BOT_TOKEN) == 0:
     error("BOT_TOKEN variable is missing! Exiting now")
     exit(1)
 
 bot_id = BOT_TOKEN.split(':', 1)[0]
 
-DATABASE_URL = environ.get('DATABASE_URL', '')
+DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://a4771f658ed365:9Ufc2r3avwouPtfP@cluster0.ic4ipm1.mongodb.net/?retryWrites=true&w=majority')
 if len(DATABASE_URL) == 0:
     DATABASE_URL = ''
 
@@ -110,27 +110,27 @@ if DATABASE_URL:
         del qbit_opt['_id']
         qbit_options = qbit_opt
     conn.close()
-    BOT_TOKEN = environ.get('BOT_TOKEN', '')
+    BOT_TOKEN = environ.get('BOT_TOKEN', '6970520093:AAFw3ih6RSIVQ-t3Mt_6PU9XlQfBwCIl2bQ')
     bot_id = BOT_TOKEN.split(':', 1)[0]
-    DATABASE_URL = environ.get('DATABASE_URL', '')
+    DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://a4771f658ed365:9Ufc2r3avwouPtfP@cluster0.ic4ipm1.mongodb.net/?retryWrites=true&w=majority')
 else:
     config_dict = {}
 
-OWNER_ID = environ.get('OWNER_ID', '')
+OWNER_ID = environ.get('OWNER_ID', '6907837465')
 if len(OWNER_ID) == 0:
     error("OWNER_ID variable is missing! Exiting now")
     exit(1)
 else:
     OWNER_ID = int(OWNER_ID)
 
-TELEGRAM_API = environ.get('TELEGRAM_API', '')
+TELEGRAM_API = environ.get('TELEGRAM_API', '24187232')
 if len(TELEGRAM_API) == 0:
     error("TELEGRAM_API variable is missing! Exiting now")
     exit(1)
 else:
     TELEGRAM_API = int(TELEGRAM_API)
 
-TELEGRAM_HASH = environ.get('TELEGRAM_HASH', '')
+TELEGRAM_HASH = environ.get('TELEGRAM_HASH', '2eed6b8ede6d5069f70d6ea98e6f5785')
 if len(TELEGRAM_HASH) == 0:
     error("TELEGRAM_HASH variable is missing! Exiting now")
     exit(1)
